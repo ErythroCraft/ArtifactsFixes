db257ae97060e5094a690b4f71bc71f088454c68
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2025-03-17 17:23:54 +0100

    Remove CompatHandler.java

--- fabric/src/main/java/artifacts/fabric/ArtifactsFabric.java
+++ fabric/src/main/java/artifacts/fabric/ArtifactsFabric.java
@@ -2,14 +2,12 @@ package artifacts.fabric;
 
 import artifacts.Artifacts;
 import artifacts.fabric.event.SwimEventsFabric;
-import artifacts.fabric.integration.CompatHandler;
 import artifacts.fabric.registry.ModFeatures;
 import artifacts.fabric.registry.ModLootTablesFabric;
 import artifacts.registry.ModItems;
 import artifacts.registry.RegistryHolder;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 
@@ -25,24 +23,11 @@ public class ArtifactsFabric implements ModInitializer {
         SwimEventsFabric.register();
         ModFeatures.register();
 
+        // TODO loot.v3
         LootTableEvents.MODIFY.register(ModLootTablesFabric::onLootTableLoad);
-
-        runCompatibilityHandlers();
     }
 
-    private void runCompatibilityHandlers() {
-        FabricLoader.getInstance().getEntrypoints("artifacts:compat_handlers", CompatHandler.class).stream()
-                .filter(handler -> FabricLoader.getInstance().isModLoaded(handler.getModId()))
-                .forEach(handler -> {
-                    String modName = FabricLoader.getInstance().getModContainer(handler.getModId())
-                            .map(container -> container.getMetadata().getName())
-                            .orElse(handler.getModId());
-                    Artifacts.LOGGER.info("Running compat handler for {}", modName);
-
-                    handler.run();
-                });
-    }
-
+    @SuppressWarnings("SameParameterValue")
     private static <R> void register(Registry<R> registry, List<RegistryHolder<R, ?>> holders) {
         holders.forEach(holder -> register(registry, holder));
     }
