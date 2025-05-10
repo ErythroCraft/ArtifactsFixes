ceab7298da92e0f39b1afa01af1709fd5bfa05b9
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2025-05-10 18:53:06 +0200

    Add back unused ability toggles data attachment to prevent log warnings

--- neoforge/src/main/java/artifacts/neoforge/registry/ModAttachmentTypes.java
+++ neoforge/src/main/java/artifacts/neoforge/registry/ModAttachmentTypes.java
@@ -2,6 +2,8 @@ package artifacts.neoforge.registry;
 
 import artifacts.Artifacts;
 import artifacts.component.SwimData;
+import com.mojang.serialization.Codec;
+import net.minecraft.util.Unit;
 import net.neoforged.neoforge.attachment.AttachmentType;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
@@ -14,5 +16,11 @@ public class ModAttachmentTypes {
 
     public static final Supplier<AttachmentType<SwimData>> SWIM_DATA = ATTACHMENT_TYPES.register("swim_data", () ->
             AttachmentType.builder(SwimData::new).build()
+    );
+
+    @Deprecated(forRemoval = true, since = "1.21.5")
+    public static final Supplier<AttachmentType<Unit>> ABILITY_TOGGLES = ATTACHMENT_TYPES.register("ability_toggles", () ->
+            AttachmentType.builder(() -> Unit.INSTANCE)
+                    .serialize(Codec.unit(Unit.INSTANCE), unit -> false).build()
     );
 }
