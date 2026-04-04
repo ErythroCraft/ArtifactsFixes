1e3dfa8189b248f73c60d12f4ec3c32671e27269
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2026-04-04 18:49:34 +0200

    Add more info to TagLoaderMixin warning

--- common/src/main/java/artifacts/mixin/debug/TagLoaderMixin.java
+++ common/src/main/java/artifacts/mixin/debug/TagLoaderMixin.java
@@ -2,34 +2,33 @@ package artifacts.mixin.debug;
 
 import artifacts.Artifacts;
 import artifacts.integration.ModCompat;
-import com.google.gson.JsonElement;
-import net.minecraft.resources.FileToIdConverter;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.tags.TagFile;
 import net.minecraft.tags.TagLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.io.Reader;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 @Mixin(TagLoader.class)
 public class TagLoaderMixin {
 
-    @SuppressWarnings("rawtypes")
-    @Inject(method = "load", require = 1, locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE", target = "Ljava/util/List;clear()V"))
-    private void detectClear(ResourceManager resourceManager, CallbackInfoReturnable<Map<ResourceLocation, List<TagLoader.EntryWithSource>>> cir, Map<ResourceLocation, List<TagLoader.EntryWithSource>> map, FileToIdConverter fileToIdConverter, Iterator var4, Map.Entry<ResourceLocation, List<Resource>> entry, ResourceLocation resourceLocation, ResourceLocation resourceLocation2, Iterator var8, Resource resource, Reader reader, JsonElement jsonElement, List<TagLoader.EntryWithSource> list, TagFile tagFile) {
+    @Inject(method = "load", require = 1, at = @At(value = "INVOKE", target = "Ljava/util/List;clear()V"))
+    private void detectClear(ResourceManager resourceManager, CallbackInfoReturnable<Map<ResourceLocation, List<TagLoader.EntryWithSource>>> cir, @Local(ordinal = 0) ResourceLocation resourceLocation, @Local Resource resource) {
         String namespace = resourceLocation.getNamespace();
 
         if (namespace.equals(ModCompat.TRINKETS) || namespace.equals(ModCompat.CURIOS) || namespace.equals(ModCompat.ACCESSORIES) || namespace.equals("artifacts")) {
-            Artifacts.LOGGER.warn("Tag entries for {} cleared by {}", resourceLocation, resource.sourcePackId());
+            Artifacts.LOGGER.warn(
+                    "Tag entries for {} cleared by {}, this is probably a bug. " +
+                            "If you're unable to equip specific items, " +
+                            "try removing the offending mod before reporting",
+                    resourceLocation, resource.sourcePackId()
+            );
         }
     }
 }
